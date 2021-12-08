130c858561b2e73180216b8c99e15a3dfe5a267b
Gunmijpizza <95372067+Gunmijpizza@users.noreply.github.com>
2021-12-08 18:32:58 +0100

    dialogue

--- Assets/Scripts/Environment/DialogueStart.cs
+++ Assets/Scripts/Environment/DialogueStart.cs
@@ -5,14 +5,14 @@ using PixelCrushers.DialogueSystem;
 
 public class DialogueStart : Interactable
 {
-    [SerializeField] private Transform TargetPosition = null;
+   
     [SerializeField] DialogueSystemTrigger dialogueStart;
 
     protected override void Interact()
     {
         if (DetectingPlayer)
         {
-            Player.position = TargetPosition.position;
+          
             dialogueStart.OnUse();
 
             base.Interact();
