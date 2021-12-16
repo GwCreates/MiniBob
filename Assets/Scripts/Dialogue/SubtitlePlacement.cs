4ff29893266d04de305ad818786fc12da55ee048
Gijs Westerdijk <gwesterdijk01@gmail.com>
2021-12-16 14:13:37 +0100

    Added repositioning dialogue boxes over character's heads when they talk

--- Assets/Scripts/Dialogue/SubtitlePlacement.cs
+++ Assets/Scripts/Dialogue/SubtitlePlacement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using PixelCrushers.DialogueSystem;
 using Sirenix.OdinInspector;
 using UnityEngine.UI;
+using CharacterInfo = PixelCrushers.DialogueSystem.CharacterInfo;
 
 public class SubtitlePlacement : MonoBehaviour
 {
+    // [SerializeField, FoldoutGroup("References")]
+    private RectTransform rectTransform;
     // [SerializeField, FoldoutGroup("References")]
     private StandardUISubtitlePanel subtitlePanel;
     // [SerializeField, FoldoutGroup("References")]
@@ -15,17 +18,50 @@ public class SubtitlePlacement : MonoBehaviour
     private Sprite SMSBackgroundLeft;
     [SerializeField, FoldoutGroup("References")]
     private Sprite SMSBackgroundRight;
+    [SerializeField, FoldoutGroup("References")]
+    private HorizontalLayoutGroup layoutGroup;
+
+    [SerializeField] private Transform speaker = null;
+
+    private RectTransformData originalLocation;
     
     [System.Serializable]
     private class ActorSubtitleData
     {
         public string actor = "null";
         public Color color = Color.grey;
+        public bool repositionSubtitle = false;
 
         public ActorSubtitleData(string name)
         {
             this.actor = name;
         }
+    }
+    
+    private class RectTransformData
+    {
+        public Vector2 pivot;
+        public Vector2 anchoredPosition;
+        public Vector2 anchorMin;
+        public Vector2 anchorMax;
+        public Vector3 position;
+
+        public RectTransformData(Vector2 pivot, Vector2 anchoredPosition, Vector2 anchorMin, Vector2 anchorMax, Vector3 position)
+        {
+            this.pivot = pivot;
+            this.anchoredPosition = anchoredPosition;
+            this.anchorMin = anchorMin;
+            this.anchorMax = anchorMax;
+            this.position = position;
+        }
+    }
+
+    void SetRectTransform(RectTransform target, RectTransformData original)
+    {
+        target.pivot = original.pivot;
+        target.anchoredPosition = original.anchoredPosition;
+        target.anchorMin = original.anchorMin;
+        target.anchorMax = original.anchorMax;
     }
 
     [SerializeField] private List<ActorSubtitleData> actorData = new List<ActorSubtitleData>();
@@ -36,6 +72,10 @@ public class SubtitlePlacement : MonoBehaviour
     {
         subtitlePanel = GetComponent<StandardUISubtitlePanel>();
         backgroundImage = GetComponent<Image>();
+        layoutGroup = GetComponent<HorizontalLayoutGroup>();
+        rectTransform = (RectTransform) transform;
+        
+        originalLocation = new RectTransformData(rectTransform.pivot, rectTransform.anchoredPosition, rectTransform.anchorMin, rectTransform.anchorMax, rectTransform.position);
     }
 
     // Update is called once per frame
@@ -43,11 +83,47 @@ public class SubtitlePlacement : MonoBehaviour
     {
         if (DialogueManager.IsConversationActive)
         {
-            string speakerName = subtitlePanel.currentSubtitle.speakerInfo.nameInDatabase;
+            CharacterInfo speakerInfo
+                = subtitlePanel.currentSubtitle.speakerInfo;
+            speaker = speakerInfo.transform;
             
-            ActorSubtitleData speakerSubtitleData = actorData.Find((x) => x.actor == speakerName) ?? defaultData;
+            ActorSubtitleData speakerSubtitleData = actorData.Find((x) => x.actor == speakerInfo.nameInDatabase) ?? defaultData;
 
             backgroundImage.color = speakerSubtitleData.color;
+
+            if (speaker != null && speakerSubtitleData.repositionSubtitle == true)
+            {
+                Vector2 speakerScreenPos = RectTransformUtility.WorldToScreenPoint(Camera.main, speaker.position);
+                bool isSpeakerLeft = speakerScreenPos.x < (Screen.width/2);
+                Debug.Log("Speaker: " + speaker + "         ScreenPos" + speakerScreenPos + "            isLeft: " + isSpeakerLeft);
+
+                if (isSpeakerLeft)
+                {
+                    // Use right background sprite
+                    backgroundImage.sprite = SMSBackgroundLeft;
+                    rectTransform.anchorMin = Vector2.zero;
+                    rectTransform.anchorMax = Vector2.zero;
+                    rectTransform.pivot = Vector2.zero;
+                    rectTransform.position = new Vector3(speakerScreenPos.x, speakerScreenPos.y, rectTransform.position.z);
+                    layoutGroup.reverseArrangement = false;
+                }
+                else
+                {
+                    // Use left background sprite
+                    backgroundImage.sprite = SMSBackgroundRight;
+                    rectTransform.anchorMin = Vector2.right;
+                    rectTransform.anchorMax = Vector2.right;
+                    rectTransform.pivot = Vector2.right;
+                    rectTransform.position = new Vector3(speakerScreenPos.x, speakerScreenPos.y, rectTransform.position.z);
+                    layoutGroup.reverseArrangement = true;
+                }
+            }
+            else
+            {
+                SetRectTransform(rectTransform, originalLocation);
+                layoutGroup.reverseArrangement = false;
+                backgroundImage.sprite = SMSBackgroundLeft;
+            }
         }
     }
 }
